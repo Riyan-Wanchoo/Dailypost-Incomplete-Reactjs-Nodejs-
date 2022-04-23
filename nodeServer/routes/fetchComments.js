14f391b15ec25601601ad4c69862215507b79c59
Riyan <riyanwanchoo1072008@yahoo.com>
2022-04-23 12:23:59 +0530

    Mapping comments

--- nodeServer/routes/fetchComments.js
+++ nodeServer/routes/fetchComments.js
@@ -3,10 +3,24 @@ const Comments = require("../models/Comments")
 const router = express.Router()
 
 router.get('/', async (req, res)=>{
+    let comments = []
+    let replies = []
     const filter = {}
     const allNews = await Comments.find(filter).sort({_id: -1})
 
-    res.json(allNews)
+    allNews.forEach((element)=>{
+        if (element.parentId==null) {
+            comments.push(element)
+        }
+        else{
+            replies.push(element)
+        }
+    })
+    let allData = {
+        comments: comments,
+        replies: replies
+    }
+    res.json(allData)
 })
 
 module.exports = router
